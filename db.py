fa8e33ee7e5df7cdf14f2b13410134b460c4e898
Nandhini <nandhini.sridhar02@gmail.com>
2024-01-15 20:53:56 -0800

    added userData.py, conditional to create table in db.py

--- db.py
+++ db.py
@@ -1,27 +1,38 @@
 import sqlite3
+import userData
 #print(sqlite3.sqlite_version)
 
 connection = sqlite3.connect("habits.db")
 
-# connection.execute('''
-# CREATE TABLE habit_db(
-#     id INTEGER PRIMARY KEY,
-#     week INTEGER ,
-#     sleep INTEGER,
-#     study INTEGER,
-#     exercise INTEGER,
-#     mood INTEGER,
-#     stress TEXT          
-# )
-# ''')
+habit_table = connection.execute("SELECT name FROM sqlite_master WHERE type=='table' AND name=='habits'").fetchall()
+if(habit_table == []):
+    connection.execute('''
+    CREATE TABLE habits(
+        id INTEGER PRIMARY KEY,
+        week INTEGER ,
+        sleep INTEGER,
+        study INTEGER,
+        exercise INTEGER,
+        mood INTEGER,
+        stress TEXT          
+    )
+    ''')
 
-test_habits = [(0, 1, 8, 4, 1, 7, "not much"), (1, 1, 6, 2, 1, 8, "tests")]
-connection.executemany("INSERT INTO habit_db VALUES(?,?,?,?,?,?,?)", test_habits)
+test_habits = [
+    (0, 1, 8, 4, 1, 7, "not much"),
+    (1, 1, 6, 2, 1, 8, "tests"),
+]
+connection.executemany("INSERT INTO habits VALUES(?,?,?,?,?,?,?)", test_habits)
 connection.commit()
 
 cur = connection.cursor()
-cur.execute("SELECT * FROM habit_db")
+cur.execute("SELECT * FROM habits")
 rows = cur.fetchall()
 for row in rows:
     print(row)
+
+#will probably take this line out later- figure out how to keep database and just keep updating it
+#connection.execute("DROP TABLE habits")
 connection.close()
+
+# connection.execute("DROP TABLE habit_db")
